ea7903b42b110c47719a43acfe37e502b5906488
Shatulskyi Vladislav <shatulsky@gmail.com>
2018-10-07 01:16:16 +0300

    license

--- SteamAutoMarket/SteamAutoMarket/MainForm.cs
+++ SteamAutoMarket/SteamAutoMarket/MainForm.cs
@@ -3,7 +3,15 @@
     using System;
     using System.Drawing;
     using System.IO;
+    using System.Linq;
+    using System.Management;
+    using System.Net;
+    using System.Text;
     using System.Windows.Forms;
+    using System.Collections.Specialized;
+
+    using Newtonsoft.Json;
+    using AutoUpdaterDotNET;
 
     public partial class MainForm : Form
     {
@@ -19,14 +27,91 @@
             this.FocusSidePanelToMenuElement(this.SettingsLinkButton);
             if (!File.Exists("license.txt"))
             {
-                throw new UnauthorizedAccessException();
+                throw new UnauthorizedAccessException("Cant get requered info");
             }
 
-            var a = File.ReadAllText("license.txt");
-            if (!Program.CheckLicense(a))
+            var main = File.ReadAllText("license.txt");
+            if (!this.Check(main))
             {
-                throw new UnauthorizedAccessException();
+                throw new UnauthorizedAccessException("Access denied");
             }
+        }
+
+        public bool Check(string main)
+        {
+            var wb = WebRequest.Create("\u0068\u0074\u0074\u0070\u0073\u003a\u002f\u002f\u0077\u0077\u0077\u002e\u0073\u0074\u0065\u0061\u006d\u0062\u0069\u007a\u002e\u0073\u0074\u006f\u0072\u0065\u002f\u0061\u0070\u0069\u002f\u0063\u0068\u0065\u0063\u006b\u006c\u0069\u0063\u0065\u006e\u0073\u0065");
+            wb.Method = "POST";
+            var data = new NameValueCollection { ["key"] = main };
+
+            // read key from user database.
+            // read from the input field if key is not present in user database
+            var mc = new ManagementClass("win32_processor");
+            var moc = mc.GetInstances();
+            var uid = moc.Cast<ManagementObject>().Select(x => x.Properties["processorID"]).FirstOrDefault()?.Value
+                .ToString();
+
+            try
+            {
+                var dsk = new ManagementObject(@"win32_logicaldisk.deviceid=""" + "C" + @":""");
+                dsk.Get();
+                uid += dsk["VolumeSerialNumber"].ToString();
+            }
+            catch
+            {
+                throw new UnauthorizedAccessException("Cant get requered info");
+            }
+
+            data["hwid"] = uid;
+            var postDataString = string.Empty;
+            foreach (string key in data)
+            {
+                postDataString += key + "&=" + data[key];
+            }
+
+            var postData = Encoding.UTF8.GetBytes(postDataString);
+            var dataStream = wb.GetRequestStream();
+            dataStream.Write(postData, 0, postData.Length);
+            dataStream.Close();
+            var resp = wb.GetResponse();
+            if (((HttpWebResponse)resp).StatusDescription != "OK")
+            {
+                throw new UnauthorizedAccessException("Access denied");
+            }
+
+            dataStream = resp.GetResponseStream();
+            var reader = new StreamReader(dataStream);
+            dynamic responseJson = JsonConvert.DeserializeObject(
+                Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(reader.ReadToEnd())));
+            dataStream.Close();
+            reader.Close();
+            resp.Close();
+            return responseJson["success_3248237582"];
+        }
+
+        private void UpdateProgram()
+        {
+            ServicePointManager.ServerCertificateValidationCallback +=
+                (sender, certificate, chain, sslPolicyErrors) => true;  // NOT FOR PRODUCTION
+                                                                        //            AutoUpdater.ApplicationExitEvent += AutoUpdater_ApplicationExitEvent;
+            AutoUpdater.RunUpdateAsAdmin = true;
+            AutoUpdater.DownloadPath = Environment.CurrentDirectory;
+            AutoUpdater.Start("https://www.steambiz.store/release/release.xml");
+        }
+
+        private void AutoUpdater_ApplicationExitEvent()
+        {
+            // let the user know that update has finished and he should launch the software again.
+
+            //            foreach (string file in Directory.EnumerateFiles(Directory.GetCurrentDirectory()))
+            //            {
+            //                try
+            //                {
+            //                    if (!file.EndsWith("SteamAutoMarket.zip")) File.Delete(file);
+            //                }
+            //                catch (Exception e) {}
+            //            }
+
+            //            Application.Exit();
         }
 
         public void AppExitButtonClick(object sender, EventArgs e)
