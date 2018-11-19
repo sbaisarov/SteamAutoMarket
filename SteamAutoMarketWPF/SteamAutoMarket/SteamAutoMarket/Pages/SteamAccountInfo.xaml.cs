5bf87dff42ee9071927282f7788c3e6685c79bee
Vladyslav_Shatulskyi <vladyslav_Shatulskyi@epam.com>
2018-11-19 14:43:11 +0200

    SteamAccountInfo empty page

---
+++ SteamAutoMarketWPF/SteamAutoMarket/SteamAutoMarket/Pages/SteamAccountInfo.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SteamAutoMarket.Pages
+{
+    /// <summary>
+    /// Interaction logic for SteamAccountInfo.xaml
+    /// </summary>
+    public partial class SteamAccountInfo : UserControl
+    {
+        public SteamAccountInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
