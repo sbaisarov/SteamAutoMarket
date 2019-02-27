aac8310a689b01dd57c8d12a63931eb066c77dc5
Vladyslav_Shatulskyi <vladyslav_Shatulskyi@epam.com>
2019-02-27 17:58:30 +0200

    Market sell localization

--- SteamAutoMarketWPF/SteamAutoMarket/SteamAutoMarket/Localization/Languages/RussianStrings.cs
+++ SteamAutoMarketWPF/SteamAutoMarket/SteamAutoMarket/Localization/Languages/RussianStrings.cs
@@ -2,13 +2,62 @@
 {
     public class RussianStrings : IStrings
     {
+        public string Button_AcceptSelectedTrades { get; } = "Принять выбранные трейды";
+
+        public string Button_ApplyFilters { get; } = "Применить\r\nфильтры";
+
+        public string Button_CancelAllListingsAwaitingConfirmation { get; } =
+            "Отменить лоты на ТП ожидающие подтверждения";
+
+        public string Button_CheckAll { get; } = "Отметить\r\nвсе";
+
+        public string Button_CheckOverpricedItems { get; } = "Отметить с завышенной ценой";
+
+        public string Button_CheckSelected { get; } = "Отметить\r\nвыбранные";
+
+        public string Button_ConfirmAllMarket2FaConfirmation { get; } = "Принять все подтверждения ТП в 2FA (SDA)";
+
+        public string Button_DeclineSelectedTrades { get; } = "Отклонить выбранные трейды";
+
         public string Button_ExtendLicense { get; } = "Продлить";
+
+        public string Button_LicenseStatus { get; } = "Статус лицензии";
+
+        public string Button_Load { get; } = "Загрузить";
+
+        public string Button_LoadMarketListings { get; } = "Загрузить лоты ТП";
+
+        public string Button_LoadTrades { get; } = "Загрузить трейды";
+
+        public string Button_Manual { get; } = "Руководство";
 
         public string Button_OpenFile { get; } = "Открыть файл";
 
         public string Button_OpenFolder { get; } = "Открыть папку";
 
+        public string Button_OpenLogFile { get; } = "Открыть файл с логами";
+
+        public string Button_RelistPrice { get; } = "Цена перевыставления";
+
+        public string Button_RelistSelected { get; } = "Перевыставить отмеченные лоты";
+
         public string Button_Reload { get; } = "Перезагрузить";
+
+        public string Button_RemoveSelected { get; } = "Снять отмеченные лоты";
+
+        public string Button_ResetFilters { get; } = "Сброситьr\n\nфильтры";
+
+        public string Button_StartMarketSell { get; } = "Начать продажу на ТП";
+
+        public string Button_SteamLogin { get; } = "Логин в Стим";
+
+        public string Button_Telegram { get; } = "Телеграм канал";
+
+        public string Button_UncheckAll { get; } = "Отменить\r\nвсе";
+
+        public string Button_Update { get; } = "Обновление";
+
+        public string Button_Website { get; } = "Вебсайт";
 
         public string MenuLink_Action { get; } = "Действия";
 
@@ -18,9 +67,9 @@
 
         public string MenuLink_AutoAccepter { get; } = "Авто подтверждение";
 
-        public string MenuLink_CurrentProcess { get; } = "Текущий процесс";
-
         public string MenuLink_Cache { get; } = "КЭШ";
+
+        public string MenuLink_CurrentProcess { get; } = "Текущий процесс";
 
         public string MenuLink_ExtendLicense { get; } = "ПРОДЛИТЬ ЛИЦЕНЗИЮ";
 
@@ -54,137 +103,138 @@
 
         public string MenuLink_Utility { get; } = "Утилиты";
 
+        public string MessageBox_RestartToApplyChanges { get; } =
+            "Перезагрузить приложение что бы применить изменения!";
+
+        public string TextBox_ActiveOnly { get; } = "Только активные";
+
+        public string TextBox_AmountToSell { get; } = "Кол-во\r\nк продаже";
+
+        public string TextBox_AppId { get; } = "App Id";
+
+        public string TextBox_AutoScrollToEnd { get; } = "Автопрокрутка до конца";
+
+        public string TextBox_AveragePrice { get; } = "Средняя цена";
+
         public string TextBox_AveragePriceDays { get; } = "Средняя цена (часы)";
 
         public string TextBox_AveragePrices { get; } = "Средняя цена";
+
+        public string TextBox_AvrPrice { get; } = "Сред. цена";
+
+        public string TextBox_ContextId { get; } = "Context Id";
+
+        public string TextBox_Count { get; } = "Кол-во";
 
         public string TextBox_CurrentLicenseKey { get; } = "Текущий ключ лицензии";
 
         public string TextBox_CurrentLicenseKeyDaysLeft { get; } = "Оставшееся кол-во дней лицензии";
 
+        public string TextBox_CurrentPrice { get; } = "Текущая цена";
+
         public string TextBox_CurrentPrices { get; } = "Текущая цена";
+
+        public string TextBox_DragColumnHeader { get; } =
+            "Перетащите заголовок столбца сюда, чтобы сгруппировать по этому столбцу";
 
         public string TextBox_ExtendLicenseKey { get; } = "Ключ продления";
 
+        public string TextBox_Id { get; } = "ID";
+
         public string TextBox_Images { get; } = "Изображения";
 
+        public string TextBox_ItemName { get; } = "Название предмета";
+
+        public string TextBox_Language { get; } = "Язык";
+
+        public string TextBox_ListedDate { get; } = "Дата выставления";
+
+        public string TextBox_ListedPrice { get; } = "Выставленная цена";
+
+        public string TextBox_LoggingDetails { get; } = "Детализация логов";
+
+        public string TextBox_ManualPrice { get; } = "Ручная цена";
+
         public string TextBox_MarketId { get; } = "ID торговой площадки";
+
+        public string TextBox_MinPrice { get; } = "Мин. цена";
+
+        public string TextBox_MyItems { get; } = "Мои предметы";
 
         public string TextBox_ObsoleteAveragePriceHours { get; } = "Устаревшая средняя цена (часы)";
 
         public string TextBox_ObsoleteCurrentPriceHours { get; } = "Устаревшая текущая цена (часы)";
 
-        public string TextBox_PriceLoadThreads { get; } = "Кол-во потоков загрузки цены";
-
-        public string TextBox_RelistThreads { get; } = "Кол-во потоков для Перевыставления";
-
-        public string TextBox_Settings { get; } = "Настройки";
-
-        public string TextBox_Theme { get; } = "Тема";
-
-        public string TextBox_TwoFactorConfirmCount { get; } = "Кол-во вещей для 2FA подтверждения";
-
-        public string TextBox_WorkingChartMaximum { get; } = "Макс. кол-во точек на графике";
-
-        public string Tooltip_GlobalTheme { get; } = "Основной цвет визуального оформления";
-
-        public string Tooltip_GlobalThemeColor { get; } = "Вторичный цвет визуального оформления";
-
-        public string Tooltip_WorkingChartMaximum { get; } =
-            "Количество точек на графике рабочего процесса для оптимизации в более компактный вид";
-
-        public string TextBox_Language { get; } = "Язык";
-
-        public string Tooltip_Language { get; } =
-            "Глобальный язык перевода. Рекомендуется перезапустить приложение после изменения данной настройки";
-
-        public string TextBox_Id { get; } = "ID";
-
-        public string TextBox_Sender { get; } = "Отправитель";
-
-        public string TextBox_State { get; } = "Состояние";
-
-        public string TextBox_MyItems { get; } = "Мои предметы";
-
-        public string TextBox_ItemName { get; } = "Название предмета";
-
-        public string TextBox_Count { get; } = "Количество";
-
-        public string TextBox_Type { get; } = "Тип";
-
         public string TextBox_Parameter { get; } = "Параметр";
-
-        public string TextBox_Value { get; } = "Значенеи";
 
         public string TextBox_PartnerItems { get; } = "Предметы партнера";
 
-        public string TextBox_SentOffers { get; } = "Отправленные трейды";
+        public string TextBox_PriceLoadThreads { get; } = "Кол-во потоков загрузки цены";
+
+        public string TextBox_Rarity { get; } = "Редкость";
+
+        public string TextBox_RealGameName { get; } = "Название игры";
 
         public string TextBox_ReceivedOffers { get; } = "Полученные трейды";
 
-        public string TextBox_ActiveOnly { get; } = "Только активные";
+        public string TextBox_RecommendedPrice { get; } = "Рекоммендуемая цена";
 
-        public string Button_LoadTrades { get; } = "Загрузить трейды";
+        public string TextBox_RelistThreads { get; } = "Кол-во потоков для Перевыставления";
 
-        public string Button_AcceptSelectedTrades { get; } = "Принять выбранные трейды";
+        public string TextBox_SelectedItemsCount { get; } = "Кол-во выбранных";
 
-        public string Button_DeclineSelectedTrades { get; } = "Отклонить выбранные трейды";
+        public string TextBox_SelectedItemsRelistPrice { get; } = "Цена перевыставления выбранных предметов";
 
-        public string TextBox_DragColumnHeader { get; } =
-            "Перетащите заголовок столбца сюда, чтобы сгруппировать по этому столбцу";
+        public string TextBox_SelectedItemsTotalPrice { get; } = "Общая стоимость выбранных товаров";
+
+        public string TextBox_SelectedTotalListedPrice { get; } = "Цена выбранных предметов";
+
+        public string TextBox_SellingPrice { get; } = "Цена продажи";
+
+        public string TextBox_Sender { get; } = "Отправитель";
+
+        public string TextBox_SentOffers { get; } = "Отправленные трейды";
+
+        public string TextBox_Settings { get; } = "Настройки";
+
+        public string TextBox_State { get; } = "Состояние";
+
+        public string TextBox_Theme { get; } = "Тема";
+
+        public string TextBox_Tradability { get; } = "Можно передать";
+
+        public string TextBox_TwoFactorConfirmCount { get; } = "Кол-во вещей для 2FA подтверждения";
+
+        public string TextBox_Type { get; } = "Тип";
 
         public string TextBox_UsefulLinks { get; } = "Полезные ссылки";
 
-        public string Button_SteamLogin { get; } = "Логин в Стим";
+        public string TextBox_Value { get; } = "Значенеи";
 
-        public string Button_LicenseStatus { get; } = "Статус лицензии";
+        public string TextBox_WorkingChartMaximum { get; } = "Макс. кол-во точек на графике";
 
-        public string Button_Update { get; } = "Обновление";
+        public string ToolTip_AddOneToAllSelected { get; } =
+            "Добавить +1 к количеству к продажи ко все выбранные предметы\r\n(Кнопка позволяет многократно запускать событие щелчка, начиная с нажатия до отпускания)";
 
-        public string Button_Telegram { get; } = "Телеграм канал";
-
-        public string Button_Website { get; } = "Вебсайт";
-
-        public string Button_Manual { get; } = "Руководство";
-
-        public string TextBox_LoggingDetails { get; } = "Детализация логов";
-
-        public string TextBox_AutoScrollToEnd { get; } = "Автопрокрутка до конца";
-
-        public string Button_OpenLogFile { get; } = "Открыть файл с логами";
-
-        public string Button_CancelAllListingsAwaitingConfirmation { get; } =
-            "Отменить лоты на ТП ожидающие подтверждения";
-
-        public string Button_ConfirmAllMarket2FaConfirmation { get; } = "Принять все подтверждения ТП в 2FA (SDA)";
-        public string TextBox_ListedDate { get; } = "Дата выставления";
-        public string TextBox_ListedPrice { get; } = "Выставленная цена";
-        public string TextBox_MinPrice { get; } = "Мин. цена";
-        public string TextBox_AvrPrice { get; } = "Сред. цена";
-        public string Button_RelistPrice { get; } = "Цена перевыставления";
-        public string Button_LoadMarketListings { get; } = "Загрузить лоты ТП";
-        public string TextBox_RecommendedPrice { get; } = "Рекоммендуемая цена";
-        public string TextBox_ManualPrice { get; } = "Ручная цена";
-        public string TextBox_AveragePrice { get; } = "Средняя цена";
-        public string TextBox_CurrentPrice { get; } = "Текущая цена";
-        public string ToolTip_ReloadAllPrices { get; } = "Перезагрузить все цены";
-        public string ToolTip_RefreshSelectedItemPrice { get; } = "Перезагрузить цену выбранного предмета";
-        public string ToolTip_StopPriceLoad { get; } = "Остановить загрузку цен";
         public string ToolTip_FindSelectedItemOnSteamMarket { get; } = "Найти выбранный предмет на ТП";
-        public string Button_CheckAll { get; } = "Отметить\r\nвсе";
-        public string Button_CheckSelected { get; } = "Отметить\r\nвыбранные";
-        public string Button_UncheckAll { get; } = "Отменить\r\nвсе";
-        public string Button_CheckOverpricedItems { get; } = "Отметить с завышенной ценой";
-        public string Button_RemoveSelected { get; } = "Снять отмеченные лоты";
-        public string Button_RelistSelected { get; } = "Перевыставить отмеченные лоты";
-        public string TextBox_SelectedItemsCount { get; } = "Кол-во выбранных";
-        public string TextBox_SelectedTotalListedPrice { get; } = "Цена выбранных предметов";
-        public string TextBox_SelectedItemsRelistPrice { get; } = "Цена перевыставления выбранных предметов";
 
-        public string Tooltip_RelistTooltip { get; } =
+        public string ToolTip_GlobalTheme { get; } = "Основной цвет визуального оформления";
+
+        public string ToolTip_GlobalThemeColor { get; } = "Вторичный цвет визуального оформления";
+
+        public string ToolTip_Language { get; } =
+            "Глобальный язык перевода. Рекомендуется перезапустить приложение после изменения данной настройки";
+
+        public string ToolTip_RelistTooltip { get; } =
             "ОБРАТИТЕ ВНИМАНИЕ!!!\r\nНевозможно обнаружить абсолютно идентичный предмет после удаления предмета из Steam Market.\r\nАлгоритм перевыставления возьмет первый предмет с соответствующим хэш именем.\r\nИспользуйте этот режим, только если на 100% уверенны в том что вам подходит такой алгоритм!";
 
-        public string MessageBox_RestartToApplyChanges { get; } =
-            "Перезагрузить приложение что бы применить изменения!";
+        public string ToolTip_ReloadAllPrices { get; } = "Перезагрузить все цены";
+
+        public string ToolTip_ReloadSelectedItemPrice { get; } = "Перезагрузить цену выбранного предмета";
+
+        public string ToolTip_StopPriceLoad { get; } = "Остановить загрузку цен";
+
+        public string ToolTip_WorkingChartMaximum { get; } =
+            "Количество точек на графике рабочего процесса для оптимизации в более компактный вид";
     }
 }
